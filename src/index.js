8b139ab5b437c9158e8c5925cbde6780a8f71f8b
Manish <manish@Manishs-MacBook-Pro.local>
2017-10-07 17:34:30 +0530

    chnage in readme file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM  from 'react-dom';
 import YTSearch   from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
 const API_KEY = "AIzaSyCRFU8tM-eyBqGXsdjYVsXcJYg20rs2Nrc";
 class App extends Component {
     constructor(props){
@@ -16,6 +17,7 @@ class App extends Component {
       return(
         <div>
             <SearchBar />
+            <VideoDetail video={this.state.videos[0]} />
             <VideoList  videos={this.state.videos} />  
         </div>
         )
